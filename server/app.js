d5ac56b27433ef5b667200f4b28cc2c5a6ff853f
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-09-07 14:03:40 +0700

    npm cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const url = 'mongodb://sukmahd:vongola12@cluster0-shard-00-00-h9kzb.mongodb.net:27017,cluster0-shard-00-01-h9kzb.mongodb.net:27017,cluster0-shard-00-02-h9kzb.mongodb.net:27017/lisica?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 mongoose.connect(url)
 
@@ -10,6 +11,8 @@ let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(cors())
 
 const user = require('./routes/user')
 const record = require('./routes/records')
