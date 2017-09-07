bfad04c075653577837c63004ee596c0cb8db7f2
sukmahd <hd.wijoyo@gmail.com>
2017-09-07 20:16:18 +0700

    voice to text

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation'
 
 import MainMenu from './screens/MainMenu';
+import Test from './screens/test_voice'
 
 const AppNavigator = StackNavigator({
 	MainMenuScreen: { screen: MainMenu }
@@ -11,7 +12,7 @@ const AppNavigator = StackNavigator({
 class App extends Component {
 	render() {
 		return(
-			<AppNavigator />
+			<Test />
 		)
 	}
 }
