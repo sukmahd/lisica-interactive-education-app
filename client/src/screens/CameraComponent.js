2df11076da900fe7f73586ad24ff085938743b5c
Patrick Benz Imanuel <PatrickBenz.09@gmail.com>
2017-09-08 15:31:35 +0700

    Upload Image to AWS S3

--- client/src/screens/CameraComponent.js
+++ client/src/screens/CameraComponent.js
@@ -9,6 +9,10 @@ import {
 } from 'react-native';
 
 import Camera from 'react-native-camera';
+import { RNS3 } from 'react-native-aws3';
+import Spinner from 'react-native-spinkit'
+
+var s3 = new AWS.S3();
 
 class CameraComponent extends Component {
   constructor(props) {
@@ -48,6 +52,47 @@ class CameraComponent extends Component {
     )
   }
 
+  cancelImage() {
+    this.setState({ path: null, isUploading: false, isProcessing: false })
+  }
+
+  renderImage () {
+    return (
+      <View>
+        <Image
+          source={{ uri: this.state.path }} style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center', width: Dimensions.get('window').width }}
+        >
+          { this.state.isUploading ?
+            <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center', backgroundColor: 'transparent' }}>
+              <Text style={{ fontWeight: 'normal', fontSize: 15, backgroundColor: 'transparent', color: '#FFF', marginBottom: 20 }}>Uploading..</Text>
+              <Spinner
+                type="9CubeGrid"
+                isVisible={ true }
+                size={ 100 }
+                color="#FFF"
+              />
+            </View>
+            : <Text></Text>
+          }
+          { this.state.isProcessing ?
+            <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center', backgroundColor: 'transparent' }}>
+              <Text style={{ fontWeight: 'normal', fontSize: 15, backgroundColor: 'transparent', color: '#FFF', marginBottom: 20 }}>Processing Image..</Text>
+              <Spinner
+                type="ChasingDots"
+                isVisible={ true }
+                size={ 100 }
+                color="#FFF"
+              />
+            </View>
+            : <Text></Text>
+          }
+          <Text onPress={ () => this.cancelImage() } style={{ position: 'absolute', top: 20, right: 20, backgroundColor: 'transparent' ,color: '#FFF', fontWeight: '600', fontSize: 20 }}>Cancel</Text>
+          <Text onPress={ () => this.uploadImageToS3() } style={{ position: 'absolute', bottom: 20, backgroundColor: 'transparent' ,color: '#FFF', fontWeight: '600', fontSize: 20 }}>haha</Text>
+        </Image>
+      </View>
+    )
+  }
+
   takePicture() {
     this.camera.capture()
       .then((data) => {
@@ -59,10 +104,47 @@ class CameraComponent extends Component {
       .catch(err => console.error(err));
   }
 
+
+  uploadImageToS3 () {
+    this.setState({ isUploading: true })
+    console.log(this.state.path);
+    let imageName = this.state.path.split("Pictures/")[1]
+    console.log(imageName);
+
+    const file = {
+      // `uri` can also be a file system path (i.e. file://)
+      uri: this.state.path,
+      name: imageName,
+      type: "image/jpg"
+    }
+
+    console.log('image sudah tertangkap');
+
+    const options = {
+      keyPrefix: "uploads/",
+      bucket: "lisica-interactive-education-app",
+      region: "us-east-1",
+      accessKey: "AKIAJBXWNUBWGAIFT2FA",
+      secretKey: "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",
+      successActionStatus: 201
+    }
+
+    console.log('bucket');
+
+    RNS3.put(file, options).then(response => {
+      console.log('hayy');
+      if (response.status !== 201) {
+        throw new Error("Failed to upload image to S3");
+        this.setState({ isUploading: false })
+      }
+      console.log(response.body);
+      this.setState({ isUploading: false })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        {this.renderCamera()}
+        {this.state.path ? this.renderImage() : this.renderCamera()}
       </View>
     );
   }
